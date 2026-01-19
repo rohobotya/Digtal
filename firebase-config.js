dcaf3751397bb535c65199ccbbbbba134156c279
rohobotya <yaikobrohobot@gmail.com>
2026-01-19 15:40:03 +0300

    Update Firebase config and add analytics
    Updated Firebase configuration and initialization to use new project credentials and added analytics support.

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,22 @@
 
-// Firebase configuration and initialization
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAiivzpIEsKhfOe2EMY3m3OxBmXjdAL8pQ",
-  authDomain: "zowi-9f8e8.firebaseapp.com",
-  databaseURL: "https://zowi-9f8e8-default-rtdb.firebaseio.com",
-  projectId: "zowi-9f8e8",
-  storageBucket: "zowi-9f8e8.firebasestorage.app",
-  messagingSenderId: "581091477130",
-  appId: "1:581091477130:web:462d677514a86f7d06cf73",
-  measurementId: "G-TCJQ1L7BGZ"
+  apiKey: "AIzaSyAlZ5d5Z5oNzPvJlRNaC7Y8LhOsEMgJMhY",
+  authDomain: "zoer-4ed04.firebaseapp.com",
+  projectId: "zoer-4ed04",
+  storageBucket: "zoer-4ed04.firebasestorage.app",
+  messagingSenderId: "887229935008",
+  appId: "1:887229935008:web:ae95be82b7a94aab5448c5",
+  measurementId: "G-PKY5Z7DT5Z"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getDatabase(app);
-
-export { auth, db };
+const analytics = getAnalytics(app);
